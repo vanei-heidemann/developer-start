e2110befe6145f5de96a931a6ab18384b4fcbd47
Vanei Anderson Heidemann <vanei.heidemann@fluig.com>
2018-11-17 11:09:58 -0200

    Adicionado instalacao do Chrome

--- developer-start.sh
+++ developer-start.sh
@@ -82,6 +82,14 @@ echo install npm
 echo ==============================
 apt-get install -y npm
 
+echo ==============================
+echo install google chrome
+echo ==============================
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google-chrome.list
+apt update
+apt install -y google-chrome-stable
+
 echo install teamviewer
 echo ==============================
 wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
