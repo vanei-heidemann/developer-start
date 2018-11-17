bb400a7abcf9e80de5fb6d6cc21093827857de68
Vanei Anderson Heidemann <vanei.heidemann@fluig.com>
2018-11-17 11:15:04 -0200

    Corrigido instacao JDk para Mint

--- developer-start.sh
+++ developer-start.sh
@@ -8,9 +8,10 @@ apt-get install wget -y
 echo ==============================
 echo install jdk
 echo ==============================
-add-apt-repository ppa:webupd8team/java && \
-echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | debconf-set-selections && \
-apt-get install -y oracle-java8-installer
+add-apt-repository ppa:webupd8team/java
+apt-get update
+apt-get install oracle-java8-installer
+apt-get install oracle-java8-set-default
 
 echo ==============================
 echo install maven
